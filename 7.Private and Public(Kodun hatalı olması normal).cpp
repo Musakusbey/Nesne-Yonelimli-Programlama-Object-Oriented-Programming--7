81966581386700cd9c288cc4bb4b5a851fdbc824
Musa Kuşbey <78588618+Musakusbey@users.noreply.github.com>
2021-02-12 15:46:20 +0300

    Add files via upload
    Nesne Yonelimli Programlama (Object-Oriented Programming)-- 7-> Public and private 
    NOT : Bu yazılan kodun hatalı çıkması bılerek yapılmıştır. Burdaki amaç private'ın nasıl işlendigini siz değerli şahıslara öğretmek

---
+++ 7.Private and Public(Kodun hatalı olması normal).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class nokta
+{
+	public:	
+	int x;
+		
+	private:	
+	int y;
+};
+   
+  int main ()
+  {
+  
+  nokta unlem;
+  
+  unlem.y;
+  
+  unlem.x;
+  
+  cout<<"x"<<unlem.x<<endl;
+  
+  cout<<"y"<<unlem.y<<endl;
+  
+  return 0;
+}
